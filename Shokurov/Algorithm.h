3e46cf0f2c5ccfeabce29ddca60639284f2f111f
FirstStep0 <75475364+FirstStep0@users.noreply.github.com>
2020-12-23 12:41:57 +0300

    Add files via upload

---
+++ Shokurov/Algorithm.h
@@ -0,0 +1,12 @@
+#pragma once
+//cортировка вставками
+unsigned long insertion_sort(double arr[], const int size);
+
+//сортировка Шелла
+unsigned long Shell_sort(double arr[], const int size);
+
+//сортировка слиянием
+unsigned long _mergesort(double arr[], const int size);
+
+//поразрядная сортировка
+unsigned long radixSort(double arr[], const long size);
