84a9a195b5f0fa92873c14b037e222a9d0f2b366
LarinK65 <75614599+LarinK65@users.noreply.github.com>
2020-12-25 16:23:36 +0300

    Update main.cpp

--- Larin K/lab_1/main.cpp
+++ Larin K/lab_1/main.cpp
@@ -1,14 +1,7 @@
 #pragma comment(linker, "/STACK:256000000")
 
-#include <iostream>
 #include "sorts.h"
 #include "menu.h"
-#include <random>
-#include <ctime>
-#include <algorithm>
-
-using namespace std;
-
 
 int main()
 {
